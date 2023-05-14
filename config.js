941a6a4f58efe1529199986239b6d1cbf72071f5
Sexjoni <133550958+Sexjoni@users.noreply.github.com>
2023-05-14 07:37:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94742632585', 'sandaru', true],
+  ['94742632585', 'senira', true], 
   [''] 
 ] //Number of owners
 
